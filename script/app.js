d33eee12847cc4d4cbc8640b2290d3bcde5075dc
hariom <hariomcuneiform@gmail.com>
2023-04-12 18:27:35 +0530

    filter open/close done

--- script/app.js
+++ script/app.js
@@ -34,3 +34,18 @@ close.addEventListener("click", () =>{
 function closeDropdown() {
   document.getElementsByClassName('dropdown-menu search-dropdown-box show')[0].classList.remove('show');  
 } 
+
+// document.getElementsByClassName("accordion-button").addEventListener("click", function(event){
+//   console.log("Eorking")
+// })
+
+console.log(document.getElementById("btnTxt"))
+
+function myFunction(className) {
+if(document.getElementsByClassName(className)[0].innerHTML == "open"){ 
+  document.getElementsByClassName(className)[0].innerHTML = "close";
+} else{
+  document.getElementsByClassName(className)[0].innerHTML = "open"
+}
+
+}
